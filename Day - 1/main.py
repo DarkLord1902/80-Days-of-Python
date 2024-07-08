ca7e4c9a9f2f09ffa5d1198073b652a3c25b6034
Vedant Jadhav <vedant.jadhav1928@yahoo.com>
2024-07-08 08:24:29 +0530

    added day01

---
+++ Day - 1/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+city = input("Which city did you grow up in?:\n")
+pet_name = input("What is the name of your first pet?:\n")
+print("Your band name is " + city + " " + pet_name)
